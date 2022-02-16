bc4f8647068c410cf140a97abac3a17f1ba56e6d
Zero† <67890978+Zeerocode@users.noreply.github.com>
2022-02-16 07:55:54 +0100

    Update tokens.ts

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -27,7 +27,7 @@ export const mainnetTokens = {
     18,
     'ZLT',
     'Zeroloss',
-    'https://metarewardstoken.com/',
+    'https://zeroloss.org/',
   ),
   // bnb here points to the wbnb contract. Wherever the currency BNB is required, conditional checks for the symbol 'BNB' can be used
   bnb: new Token(MAINNET, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'BNB', 'BNB', 'https://www.binance.com/'),
